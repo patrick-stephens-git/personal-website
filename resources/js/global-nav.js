a2d114832e59176f1b461596fc8e00c2e5b11593
Patrick Stephens <patrickcstephens@me.com>
2024-09-02 14:50:29 -0600

    Add files via upload

--- resources/js/global-nav.js
+++ resources/js/global-nav.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', function() {
         { text: 'Notes', url: '/posts', target: '_self' },
         { text: 'LinkedIn', url: 'https://www.linkedin.com/in/patrickcstephens/', target: '_blank' },
         { text: 'Git', url: 'https://github.com/patrick-stephens-git', target: '_blank' },
-        { text: 'SoundCloud', url: 'https://soundcloud.com/user-579514550', target: '_blank' }
+        { text: 'SoundCloud', url: 'https://soundcloud.com/user-579514550', target: '_blank' },
+        { text: 'Contact', url: 'mailto:patrickcstephens@me.com?subject=Reaching%20out%20from%20patrickcstephens.com', target: '_self' } 
     ];
 
     // create <nav></nav> element
