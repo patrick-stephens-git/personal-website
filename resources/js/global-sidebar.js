783d34e97911a472c935aa3023b4d8d91e61db14
Patrick Stephens <patrickcstephens@me.com>
2024-09-02 20:15:16 -0600

    Add files via upload

--- resources/js/global-sidebar.js
+++ resources/js/global-sidebar.js
@@ -111,7 +111,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // links
     const links_d = [
         { text: 'mapping incorrect|correct spelling pairs', url: 'https://github.com/patrick-stephens-git/data-mining-tools/tree/main/spelling-error-to-keyword-pairs-generator', target: '_blank' },
-        { text: 'mapping query|acronym spelling pairs', url: 'https://github.com/patrick-stephens-git/data-mining-tools/tree/main/query-refinements-to-keyword-acronym-pairs-generator', target: '_blank' }
+        { text: 'mapping query|acronym pairs', url: 'https://github.com/patrick-stephens-git/data-mining-tools/tree/main/query-refinements-to-keyword-acronym-pairs-generator', target: '_blank' }
     ];
 
     // for loop to create sidebar links as <li> elements
