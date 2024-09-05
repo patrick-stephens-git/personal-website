d1a2ca849b391bf8ae85330e69b31b8f97e1ebc8
Patrick Stephens <patrickcstephens@me.com>
2024-09-04 23:35:36 -0600

    Add files via upload

--- resources/js/global-sidebar.js
+++ resources/js/global-sidebar.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // links
     const links_a = [
         { text: 'product strategy', url: '/posts/product-strategy.html', target: '_self' } //,
-        // { text: 'product vision', url: 'https://docs.google.com/document/d/1T_OP7Fo59u6OJlR25AxuSiWjV2oc6vY6UdvzS7B48aA/edit#heading=h.b7hj61f3essg', target: '_blank' },
+        // { text: 'search from a product perspective', url: '/posts/search-from-a-product-perspective.html', target: '_self' },
         // { text: 'understanding users', url: 'https://docs.google.com/document/d/1T_OP7Fo59u6OJlR25AxuSiWjV2oc6vY6UdvzS7B48aA/edit#heading=h.tgp3vpvgg2nv', target: '_blank' },
         // { text: 'surfacing problems', url: 'https://docs.google.com/document/d/1T_OP7Fo59u6OJlR25AxuSiWjV2oc6vY6UdvzS7B48aA/edit#heading=h.32ulggd979n', target: '_blank' },
         // { text: 'testing assumptions', url: 'https://docs.google.com/document/d/1T_OP7Fo59u6OJlR25AxuSiWjV2oc6vY6UdvzS7B48aA/edit#heading=h.8au30ko8im4k', target: '_blank' },
@@ -49,16 +49,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // links
     const links_b = [
-        { text: 'understanding search', url: 'https://docs.google.com/document/d/1Z47NT1j-UKlA_JjNPEVAwjLYql2hh7Rs9WA0gOPFsi0/edit?usp=sharing', target: '_blank' },
-        { text: 'document understanding', url: 'https://docs.google.com/document/d/1Z47NT1j-UKlA_JjNPEVAwjLYql2hh7Rs9WA0gOPFsi0/edit#heading=h.5i47x3protnq', target: '_blank' },
-        { text: 'query understanding', url: 'https://docs.google.com/document/d/1Z47NT1j-UKlA_JjNPEVAwjLYql2hh7Rs9WA0gOPFsi0/edit#heading=h.1zstsgw5m4pd', target: '_blank' },
-        { text: 'user understanding', url: 'https://docs.google.com/document/d/1Z47NT1j-UKlA_JjNPEVAwjLYql2hh7Rs9WA0gOPFsi0/edit#heading=h.lafdf2udi3ay', target: '_blank' },
-        { text: 'information retrieval', url: 'https://docs.google.com/document/d/1Z47NT1j-UKlA_JjNPEVAwjLYql2hh7Rs9WA0gOPFsi0/edit#heading=h.4d8ubu8yd08p', target: '_blank' },
-        { text: 'ranking search results', url: 'https://docs.google.com/document/d/1Z47NT1j-UKlA_JjNPEVAwjLYql2hh7Rs9WA0gOPFsi0/edit#heading=h.nu8vjvlsjd8n', target: '_blank' },
-        { text: 'manual overrides', url: 'https://docs.google.com/document/d/1Z47NT1j-UKlA_JjNPEVAwjLYql2hh7Rs9WA0gOPFsi0/edit#heading=h.o05x8wf70848', target: '_blank' },
-        { text: 'analyzing search results', url: 'https://docs.google.com/document/d/1Z47NT1j-UKlA_JjNPEVAwjLYql2hh7Rs9WA0gOPFsi0/edit#heading=h.ll9n8l37ziqw', target: '_blank' },
-        { text: 'search ux', url: 'https://docs.google.com/document/d/1Z47NT1j-UKlA_JjNPEVAwjLYql2hh7Rs9WA0gOPFsi0/edit#heading=h.oaersfeucrzu', target: '_blank' },
-        { text: 'ai in search', url: 'https://docs.google.com/document/d/1Z47NT1j-UKlA_JjNPEVAwjLYql2hh7Rs9WA0gOPFsi0/edit#heading=h.utf4de74ch92', target: '_blank' }
+        { text: 'search from a product perspective', url: '/posts/search-from-a-product-perspective.html', target: '_self' } //,
+        // { text: 'document understanding', url: 'xxxx', target: '_blank' },
     ];
 
     // for loop to create sidebar links as <li> elements
@@ -131,8 +123,8 @@ document.addEventListener('DOMContentLoaded', function() {
     sidebarBar.appendChild(h2a);
     sidebarBar.appendChild(ula);
 
-    // sidebarBar.appendChild(h2b);
-    // sidebarBar.appendChild(ulb);
+    sidebarBar.appendChild(h2b);
+    sidebarBar.appendChild(ulb);
 
     // sidebarBar.appendChild(h2c);
     // sidebarBar.appendChild(ulc);
