2a1238ea0a26f25a4fe306ebc5fdf29bfc22e5d0
patrick-stephens-git <patrickcstephens@me.com>
2025-05-01 11:39:00 -0600

    updated variables

--- resources/js/years-experience.js
+++ resources/js/years-experience.js
@@ -2,24 +2,25 @@ document.addEventListener('DOMContentLoaded', function() {
     // define dates
     const careerStartDate = new Date('2014-06-01');
     const saasStartDate = new Date('2016-07-01');
+    const saasEndDate = new Date('2025-04-30');
     const productStartDate = new Date('2019-04-01');
+    const productEndDate = new Date('2025-04-30');
 
-    function calculateYears(startDate) {
-        const today = new Date(); // current date
-        let yearDifference = today.getFullYear() - startDate.getFullYear(); // difference between current year and starting year
-        const monthDifference = today.getMonth() - startDate.getMonth(); // difference between current month and starting month
+    function calculateYears(startDate, endDate = new Date()) { // default endDate is current date
+        let yearDifference = endDate.getFullYear() - startDate.getFullYear(); // difference between end year and starting year
+        const monthDifference = endDate.getMonth() - startDate.getMonth(); // difference between end month and starting month
 
-        if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < startDate.getDate())) {
-            yearDifference--; // decrement years (reduce by 1) if monthDifference is < 0 OR if monthDifference is = 0 if today's date is < startDate
+        if (monthDifference < 0 || (monthDifference === 0 && endDate.getDate() < startDate.getDate())) {
+            yearDifference--; // decrement years (reduce by 1) if monthDifference is < 0 OR if monthDifference is = 0 if endDate is < startDate
         }
 
         return yearDifference;
     }
 
-    function checkElementById(id, startDate) {
+    function checkElementById(id, startDate, endDate = new Date()) { // default endDate is current date
         const container = document.getElementById(id); // gets existing element from html with id="id"
         if (container) { // if container exists 
-            document.getElementById(id).textContent = calculateYears(startDate); // add text to container
+            document.getElementById(id).textContent = calculateYears(startDate, endDate); // add text to container
             console.log(`${id} added successfully`); // log: success
         } else { // if container does not exist
             console.log(`${id} not found`); // log: failure
@@ -27,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     checkElementById('career-years-experience', careerStartDate);
-    checkElementById('saas-years-experience', saasStartDate);
-    checkElementById('product-years-experience', productStartDate);
+    checkElementById('saas-years-experience', saasStartDate, saasEndDate);
+    checkElementById('product-years-experience', productStartDate, productEndDate);
 
 });
