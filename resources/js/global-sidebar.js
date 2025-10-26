163c02d978fe99405864592956e5f8eb53c16dce
patrick-stephens-git <patrickcstephens@me.com>
2025-10-26 12:37:13 -0500

    removed services pages

--- resources/js/global-sidebar.js
+++ resources/js/global-sidebar.js
@@ -12,27 +12,27 @@ document.addEventListener('DOMContentLoaded', function() {
     const sidebarBar = document.createElement('sidebar'); // create element: <sidebar></sidebar>
     sidebarBar.className = 'global-sidebar'; // add class: <sidebar class="global-sidebar"></sidebar>
 
-    const h2_services = document.createElement('h2'); // create element: <h2></h2>
-    h2_services.textContent = 'Services'; // textContent treats text as plain text
+    // const h2_services = document.createElement('h2'); // create element: <h2></h2>
+    // h2_services.textContent = 'Services'; // textContent treats text as plain text
 
-    const ul_services = document.createElement('ul'); // create element: <ul></ul>
+    // const ul_services = document.createElement('ul'); // create element: <ul></ul>
 
-    const links_services = [
-        { text: 'SEO Team Training', url: '/services/seo-team-training.html', target: '_self' }
-    ];
+    // const links_services = [
+    //     { text: 'SEO Team Training', url: '/services/seo-team-training.html', target: '_self' }
+    // ];
 
-    links_services.forEach(link => {
-        const li = document.createElement('li'); // create element: <li></li>
-        const a = document.createElement('a'); // create element: <a></a>
-        a.href = link.url; // add attribute: <a href="link.url"></a>
-        a.textContent = link.text; // add text: <a href="link.url">text</a>
-        a.target = link.target; // add target: <a href="link.url" target="link.target">text</a>
-        li.appendChild(a); // append link to list item element: <li><a href="link.url" target="link.target">text</a></li>
-        ul_services.appendChild(li); // append list item to unordered list element: <ul><li><a href="link.url" target="link.target">text</a></li></ul>
-    });
+    // links_services.forEach(link => {
+    //     const li = document.createElement('li'); // create element: <li></li>
+    //     const a = document.createElement('a'); // create element: <a></a>
+    //     a.href = link.url; // add attribute: <a href="link.url"></a>
+    //     a.textContent = link.text; // add text: <a href="link.url">text</a>
+    //     a.target = link.target; // add target: <a href="link.url" target="link.target">text</a>
+    //     li.appendChild(a); // append link to list item element: <li><a href="link.url" target="link.target">text</a></li>
+    //     ul_services.appendChild(li); // append list item to unordered list element: <ul><li><a href="link.url" target="link.target">text</a></li></ul>
+    // });
 
-    sidebarBar.appendChild(h2_services); // append h2 to sidebar: <sidebar><h2>textContent</h2></sidebar>
-    sidebarBar.appendChild(ul_services); // append ul to sidebar: <sidebar><h2>textContent</h2><ul><li><a href="" target=""></a></li>...<li><a href="" target=""></a></li></ul></sidebar>
+    // sidebarBar.appendChild(h2_services); // append h2 to sidebar: <sidebar><h2>textContent</h2></sidebar>
+    // sidebarBar.appendChild(ul_services); // append ul to sidebar: <sidebar><h2>textContent</h2><ul><li><a href="" target=""></a></li>...<li><a href="" target=""></a></li></ul></sidebar>
     
     const h2_posts = document.createElement('h2'); // create element: <h2></h2>
     h2_posts.textContent = 'Posts'; // textContent treats text as plain text
